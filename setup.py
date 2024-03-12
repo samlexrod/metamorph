2736f6ddaa6bef202ed02ec5f0aed73c95f5c7ad
Samuel Rodriguez <31121205+samlexrod@users.noreply.github.com>
2024-03-12 16:27:52 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="metamorph",
+    version="0.1.0",
+    author="Samuel Rodriguez",
+    author_email="rodsamuel@outlook.com",
+    description="A package for de-identifying data, transforming it for privacy.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/samlexrod/metamorph",
+    packages=find_packages(),
+    install_requires=[
+        scrubadub=="2.0.0"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
